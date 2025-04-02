2b922ff4333c06f6c05f5db0984ba8dd7577797f
SzymonZasada <84040786+SzymonZasada@users.noreply.github.com>
2025-04-02 09:07:55 +0200

    Update deploy-aws.sh

--- deploy-scripts/deploy-aws.sh
+++ deploy-scripts/deploy-aws.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 set -e
 
-echo "Rozpoczynam wdrażanie na AWS..."
+echo "Rozpoczynam wdrażanie na lokalnym serwerze..."
 
-# Logowanie do AWS ECR (Elastic Container Registry)
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com
+# Zatrzymaj istniejący kontener (jeśli istnieje)
+docker stop ${APP_NAME} || true
+docker rm ${APP_NAME} || true
 
-# Tagowanie obrazu dla ECR
-docker tag ${APP_NAME}:${BUILD_VERSION} ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/${APP_NAME}:${BUILD_VERSION}
-
-# Wysyłanie obrazu do ECR
-docker push ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/${APP_NAME}:${BUILD_VERSION}
-
-# Aktualizacja definicji zadania ECS
-envsubst < task-definition-template.json > task-definition.json
-aws ecs register-task-definition --cli-input-json file://task-definition.json
-
-# Aktualizacja usługi ECS
-aws ecs update-service --cluster ${ECS_CLUSTER} --service ${ECS_SERVICE} --task-definition ${APP_NAME}:${BUILD_VERSION} --force-new-deployment
-
-echo "Wdrażanie na AWS zakończone."
+# Uruchom nowy kontener
+docker run -d --name ${APP_NAME} -p 80:80 ${APP_NAME}:${BUILD_VERSION}
